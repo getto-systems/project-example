366e63ac6798c27529d6e4765203f704030a0d03
shun <shun.fix9@gmail.com>
2021-06-13 15:30:36 +0900

    refactor: get current method

--- src/avail/version/get_current/method.ts
+++ src/avail/version/get_current/method.ts
@@ -1,5 +1,17 @@
-import { VersionString } from "../data";
+import { GetCurrentVersionInfra } from "./infra"
+
+import { versionStringConfigConverter } from "../converter"
+
+import { VersionString } from "../data"
 
 export interface GetCurrentVersionMethod {
     (): VersionString
+}
+
+interface GetCurrentVersion {
+    (infra: GetCurrentVersionInfra): GetCurrentVersionMethod
+}
+export const getCurrentVersion: GetCurrentVersion = (infra) => () => {
+    const { version } = infra
+    return versionStringConfigConverter(version)
 }
