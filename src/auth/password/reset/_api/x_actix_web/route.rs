03eda6ceac6309a86311f3bbe24f8515290aef7c
shun <shun.fix9@gmail.com>
2021-06-23 02:38:50 +0900

    add: reset route

--- src/auth/password/reset/_api/x_actix_web/route.rs
+++ src/auth/password/reset/_api/x_actix_web/route.rs
@@ -2,16 +2,18 @@ use actix_web::{post, web, HttpRequest, Responder, Scope};
 
 use getto_application::helper::flatten;
 
-use crate::{
-    auth::password::reset::_api::action_request_token::action::RequestResetTokenAction,
-    x_outside_feature::_api::{
-        state::AppData,
-        logger::{app_logger, Logger},
-    },
+use crate::x_outside_feature::_api::{
+    logger::{app_logger, Logger},
+    state::AppData,
+};
+
+use crate::auth::password::reset::_api::{
+    action_request_token::action::RequestResetTokenAction,
+    action_reset::action::ResetPasswordAction,
 };
 
 pub fn scope_reset() -> Scope {
-    web::scope("/reset").service(request_token)
+    web::scope("/reset").service(request_token).service(reset)
 }
 
 #[post("/token")]
@@ -20,4 +22,12 @@ async fn request_token(data: AppData, request: HttpRequest, body: String) -> imp
     let mut action = RequestResetTokenAction::new(&data.auth, &request, body);
     action.subscribe(move |state| logger.log(state.log_level(), state));
     flatten(action.ignite().await).respond_to(&request)
+}
+
+#[post("")]
+async fn reset(data: AppData, request: HttpRequest, body: String) -> impl Responder {
+    let logger = app_logger(&request);
+    let mut action = ResetPasswordAction::new(&data.auth, &request, body);
+    action.subscribe(move |state| logger.log(state.log_level(), state));
+    flatten(action.ignite()).respond_to(&request)
 }
