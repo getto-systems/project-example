740108dc0913c55372bfaad21cfe09a732aac385
shun <shun.fix9@gmail.com>
2021-07-03 08:33:00 +0000

    add: sql docker-wrapper

--- .docker-wrapper.sh
+++ .docker-wrapper.sh
@@ -1,8 +1,15 @@
 DOCKER_WRAPPER_IMAGE_node(){ head -1 $APP_ROOT/.gitlab-ci.yml | sed "s/.*node:\(.*\)/\1/"; }
-DOCKER_WRAPPER_IMAGE_debian(){ echo "buster"; }
+DOCKER_WRAPPER_IMAGE_cloudsql(){ echo "gcr.io/cloudsql-docker/gce-proxy:1.23.0"; }
 
 DOCKER_WRAPPER_APP(){
   case $1 in
+    sql)
+      command=$2
+      if [ -z "$command" ]; then
+        command=restart
+      fi
+      cloudsql-server $1 "$command" /cloud_sql_proxy -instances=${SQL_INSTANCE}=tcp:0.0.0.0:3306 -credential_file=${SQL_CREDENTIAL}
+      ;;
     public|secure)
       command=$2
       if [ -z "$command" ]; then
@@ -21,5 +28,6 @@ DOCKER_WRAPPER_APP(){
       ;;
   esac
 }
+DOCKER_WRAPPER_SERVER_OPTS_sql(){ echo "-p ${LABO_PORT_PREFIX}${SQL_PORT}:3306"; }
 DOCKER_WRAPPER_SERVER_OPTS_public(){ echo "-p ${LABO_PORT_PREFIX}${PUBLIC_PORT}:${PUBLIC_APP_PORT}"; }
 DOCKER_WRAPPER_SERVER_OPTS_secure(){ echo "-p ${LABO_PORT_PREFIX}${SECURE_PORT}:${SECURE_APP_PORT}"; }
