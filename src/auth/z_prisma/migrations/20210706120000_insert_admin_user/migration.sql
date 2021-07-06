acbd16738a10f20193bf3e42671540f901ce716b
shun <shun.fix9@gmail.com>
2021-07-06 23:06:44 +0900

    fix: admin user id

--- src/auth/z_prisma/migrations/20210706120000_insert_admin_user/migration.sql
+++ src/auth/z_prisma/migrations/20210706120000_insert_admin_user/migration.sql
@@ -1,4 +1,4 @@
-insert into user(user_id, login_id) values('58f989a5-f582-49b2-a0a7-87b9abdc2186', 'admin');
-insert into user_granted_role(user_id, role) values('58f989a5-f582-49b2-a0a7-87b9abdc2186', 'admin');
-insert into user_granted_role(user_id, role) values('58f989a5-f582-49b2-a0a7-87b9abdc2186', 'dev-docs');
-insert into user_password_reset_token_destination(user_id, email) values('58f989a5-f582-49b2-a0a7-87b9abdc2186', 'shun@getto.systems');
+insert into user(user_id, login_id) values(uuid(), 'admin');
+insert into user_granted_role(user_id, role) select user_id, 'admin' from user where login_id = 'admin';
+insert into user_granted_role(user_id, role) select user_id, 'dev-docs' from user where login_id = 'admin';
+insert into user_password_reset_token_destination(user_id, email) select user_id, 'shun@getto.systems' from user where login_id = 'admin';
