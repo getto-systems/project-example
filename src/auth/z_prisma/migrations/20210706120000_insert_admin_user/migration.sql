eb87898d4782a6a1af5ed94d49c59f756e56b613
shun <shun.fix9@gmail.com>
2021-07-06 22:58:49 +0900

    add: admin user seed

---
+++ src/auth/z_prisma/migrations/20210706120000_insert_admin_user/migration.sql
@@ -0,0 +1,4 @@
+insert into user(user_id, login_id) values('58f989a5-f582-49b2-a0a7-87b9abdc2186', 'admin');
+insert into user_granted_role(user_id, role) values('58f989a5-f582-49b2-a0a7-87b9abdc2186', 'admin');
+insert into user_granted_role(user_id, role) values('58f989a5-f582-49b2-a0a7-87b9abdc2186', 'dev-docs');
+insert into user_password_reset_token_destination(user_id, email) values('58f989a5-f582-49b2-a0a7-87b9abdc2186', 'shun@getto.systems');
