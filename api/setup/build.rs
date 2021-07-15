9e7d343290b6055f25324256c5194c7dea333e8d
shun <shun.fix9@gmail.com>
2021-07-15 21:45:29 +0900

    add: tonic demo

--- api/setup/build.rs
+++ api/setup/build.rs
@@ -1,7 +1,9 @@
 mod protobuf;
+mod grpc;
 mod environment;
 
 fn main() {
+    grpc::generate("auth");
     protobuf::generate("auth.auth_ticket");
     protobuf::generate("auth.password");
     protobuf::generate("auth.password.reset");
