9edf350a458fdd10e0686ac3ef588aaba6652588
shun <shun.fix9@gmail.com>
2022-01-24 22:23:19 +0900

    refactor: response

--- src/auth/user/password/remote/authenticate/x_tonic/response.rs
+++ src/auth/user/password/remote/authenticate/x_tonic/response.rs
@@ -6,7 +6,10 @@ use crate::z_lib::remote::response::tonic::RespondTo;
 
 use super::super::action::{AuthenticatePasswordEvent, AuthenticatePasswordState};
 
+use crate::auth::ticket::remote::encode::event::EncodeAuthTicketEvent;
+
 use super::super::data::AuthenticatePasswordError;
+use crate::auth::ticket::remote::encode::data::AuthTicketEncoded;
 
 impl RespondTo<AuthenticatePasswordResponsePb> for AuthenticatePasswordState {
     fn respond_to(self) -> Result<Response<AuthenticatePasswordResponsePb>, Status> {
@@ -16,6 +19,28 @@ impl RespondTo<AuthenticatePasswordResponsePb> for AuthenticatePasswordState {
             Self::Issue(event) => event.respond_to(),
             Self::Encode(event) => event.respond_to(),
         }
+    }
+}
+
+impl RespondTo<AuthenticatePasswordResponsePb> for EncodeAuthTicketEvent {
+    fn respond_to(self) -> Result<Response<AuthenticatePasswordResponsePb>, Status> {
+        match self {
+            Self::TokenExpiresCalculated(_) => Err(Status::cancelled("token expires calculated")),
+            Self::Success(response) => response.respond_to(),
+            Self::TicketNotFound => Err(Status::unauthenticated("ticket not found")),
+            Self::RepositoryError(err) => err.respond_to(),
+            Self::EncodeError(err) => err.respond_to(),
+        }
+    }
+}
+
+impl RespondTo<AuthenticatePasswordResponsePb> for AuthTicketEncoded {
+    fn respond_to(self) -> Result<Response<AuthenticatePasswordResponsePb>, Status> {
+        Ok(Response::new(AuthenticatePasswordResponsePb {
+            success: true,
+            user: Some(self.user.into()),
+            token: Some(self.token.into()),
+        }))
     }
 }
 
