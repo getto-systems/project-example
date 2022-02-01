55c19fe10cada8ce65b7639e80d376e5c66004ba
shun <shun.fix9@gmail.com>
2022-02-02 07:31:09 +0900

    refactor: init

--- src/auth/sign/sign/init/shell.ts
+++ src/auth/sign/sign/init/shell.ts
@@ -1,12 +1,14 @@
 import { LocationOutsideFeature } from "../../../../z_lib/ui/location/feature"
 
-import { newSignViewTypeDetecter } from "../../router/init/init"
+import { toURL } from "../../../../z_lib/ui/location/init"
+
+import { detectSignViewType } from "../../router/convert"
 
 import { SignActionShell } from "../action"
 
 type OutsideFeature = LocationOutsideFeature
 export function newSignActionShell(feature: OutsideFeature): SignActionShell {
     return {
-        detectViewType: newSignViewTypeDetecter(feature),
+        detectViewType: () => detectSignViewType(toURL(feature)),
     }
 }
