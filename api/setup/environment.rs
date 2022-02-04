ea79d88598276ee00876c70082d1760682a14c7a
shun <shun.fix9@gmail.com>
2022-02-04 19:49:16 +0900

    fix: env

--- api/setup/environment.rs
+++ api/setup/environment.rs
@@ -1,41 +1,19 @@
 use std::{
     fs::{read_to_string, File},
     io::Write,
-    path::{Path, PathBuf},
+    path::PathBuf,
 };
 
 pub fn generate() {
-    EnvironmentCodegen::new()
-        .build()
-        .expect("failed to build environment");
+    build().expect("failed to build environment");
 }
 
-struct EnvironmentCodegen {
-    file: PathBuf,
-}
-
-impl EnvironmentCodegen {
-    fn new() -> Self {
-        Self {
-            file: Path::new("src/y_environment/api").join("env.rs"),
-        }
-    }
-
-    fn build(&self) -> std::io::Result<()> {
-        self.env()?;
-        Ok(())
-    }
-
-    fn env(&self) -> std::io::Result<()> {
-        let mut file = File::create(self.file.as_path())?;
-        write!(file, "{}", env_content()?)?;
-        file.flush()
-    }
-}
-
-fn env_content() -> std::io::Result<String> {
+fn build() -> std::io::Result<()> {
     let version = read_to_string("api/VERSION")?;
-    Ok(CONTENT.replace("{VERSION}", version.trim_end()))
+
+    let mut file = File::create(PathBuf::from("src/y_environment/api/env.rs"))?;
+    write!(file, "{}", CONTENT.replace("{VERSION}", version.trim_end()))?;
+    file.flush()
 }
 
 const CONTENT: &'static str = r#####"pub const VERSION: &'static str = "{VERSION}";
