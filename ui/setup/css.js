4c2a028e074eac462f4d5eff4b7b8406a9f1f967
shun <shun.fix9@gmail.com>
2022-02-04 09:52:38 +0900

    refactor: css

--- ui/setup/css.js
+++ ui/setup/css.js
@@ -8,7 +8,7 @@ const autoprefixer = require("autoprefixer")
 const CleanCss = require("clean-css")
 
 compile({
-    source: path.join(__dirname, "../css/site.css"),
+    source: path.join(__dirname, "../../src/css/site.css"),
     output: path.join(__dirname, "../public/dist/css/site.css"),
     plugins: [autoprefixer()],
 })
