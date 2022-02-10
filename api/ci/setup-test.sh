b943681d3a750bedbfb6b9cab61a025221828a83
shun <shun.fix9@gmail.com>
2022-02-10 17:27:33 +0900

    fix: setup test

--- api/ci/setup-test.sh
+++ api/ci/setup-test.sh
@@ -5,7 +5,6 @@ setup_main() {
     setup_grcov
 }
 setup_toolchain() {
-    apt-get install -y bzip2
     rustup install nightly
     rustup component add --toolchain nightly llvm-tools-preview
     rustup component add --toolchain nightly rustfmt
@@ -19,8 +18,8 @@ setup_grcov() {
             grep '"tag_name":' |
             sed -E 's/.*"v([^"]+)".*/\1/'
     )
-    curl -sSL https://github.com/mozilla/grcov/releases/download/v${version}/grcov-linux-x86_64.tar.bz2 > tmp/grcov.tar.bz2
-    tar -xvjf tmp/grcov.tar.bz2
+    curl -sSL https://github.com/mozilla/grcov/releases/download/v${version}/grcov-v${version}-x86_64-unknown-linux-gnu.tar.gz > tmp/grcov.tar.gz
+    tar -xvzf tmp/grcov.tar.gz
 
     mv grcov ${CARGO_HOME}/bin
 }
