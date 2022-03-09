08db8299cb4443b803007a06d8370d0766ba9dcd
shun <shun.fix9@gmail.com>
2022-03-09 16:00:23 +0900

    fix: scroll to focus

--- src/auth/user/account/search/x_preact/list.ts
+++ src/auth/user/account/search/x_preact/list.ts
@@ -7,14 +7,11 @@ import { box_grow, container } from "../../../../../z_vendor/getto-css/preact/de
 import { SearchAuthUserAccountPagerEntry } from "./pager"
 import { SearchAuthUserAccountTableEntry } from "./table"
 
-import {
-    DetailAuthUserAccountAction,
-    DetailAuthUserAccountState,
-    ListAuthUserAccountAction,
-} from "../action"
+import { DetailAuthUserAccountAction, ListAuthUserAccountAction } from "../action"
 
 import { useAuthUserAccountTableStructure } from "./structure"
 import { useApplicationAction } from "../../../../../z_vendor/getto-application/action/x_preact/hooks"
+import { scrollToFocused } from "../../../../../z_lib/ui/search/sidebar/x_preact/helper"
 
 type EntryProps = Readonly<{
     list: ListAuthUserAccountAction
@@ -32,63 +29,10 @@ export function ListAuthUserAccountEntry(resource: EntryProps): VNode {
 function useScrollToFocused(detail: DetailAuthUserAccountAction): void {
     const state = useApplicationAction(detail)
     useEffect(() => {
-        scrollToFocused(state)
+        scrollToFocused({
+            sidebarId: "sidebar",
+            focusedId: "focused",
+            isFirstTime: state.type === "focus-detected",
+        })
     }, [state])
-}
-
-function scrollToFocused(state: DetailAuthUserAccountState) {
-    const sidebar = document.getElementById("sidebar")
-    const focused = document.getElementById("focused")
-    if (sidebar && focused) {
-        const props: ScrollDiffProps = {
-            sidebar: sidebar.getBoundingClientRect(),
-            focused: focused.getBoundingClientRect(),
-        }
-        const threshold = 0.175
-
-        if (state.type === "focus-detected") {
-            sidebar.scrollTop = scrollTop({
-                currentScrollTop: 0,
-                scrollDiff: scrollDiff(props, threshold),
-            })
-            return
-        }
-
-        const scrollUnder = scrollDiff(props, threshold)
-        if (scrollUnder < 0) {
-            sidebar.scrollTop = scrollTop({
-                currentScrollTop: sidebar.scrollTop,
-                scrollDiff: scrollUnder,
-            })
-            return
-        }
-
-        const scrollOver = scrollDiff(props, 1 - threshold)
-        if (scrollOver > 0) {
-            sidebar.scrollTop = scrollTop({
-                currentScrollTop: sidebar.scrollTop,
-                scrollDiff: scrollOver,
-            })
-            return
-        }
-    }
-
-    type ScrollDiffProps = Readonly<{
-        sidebar: DOMRect
-        focused: DOMRect
-    }>
-    function scrollDiff({ sidebar, focused }: ScrollDiffProps, threshold: number): number {
-        return focused.top - (sidebar.top + sidebar.height * threshold)
-    }
-    function scrollTop({
-        currentScrollTop,
-        scrollDiff,
-    }: Readonly<{ currentScrollTop: number; scrollDiff: number }>): number {
-        const scrollTop = currentScrollTop + scrollDiff
-        if (scrollTop < 0) {
-            return 0
-        } else {
-            return scrollTop
-        }
-    }
 }
--- src/z_lib/ui/search/sidebar/x_preact/helper.ts
+++ src/z_lib/ui/search/sidebar/x_preact/helper.ts
@@ -11,3 +11,65 @@ export function sidebarExpand(state: SearchSidebarState): boolean {
             return state.state.isExpand
     }
 }
+
+export type ScrollToFocusedProps = Readonly<{
+    sidebarId: string
+    focusedId: string
+    isFirstTime: boolean
+}>
+export function scrollToFocused({ sidebarId, focusedId, isFirstTime }: ScrollToFocusedProps) {
+    const sidebar = document.getElementById(sidebarId)
+    const focused = document.getElementById(focusedId)
+    if (sidebar && focused) {
+        const props: ScrollDiffProps = {
+            sidebar: sidebar.getBoundingClientRect(),
+            focused: focused.getBoundingClientRect(),
+        }
+        const threshold = 0.175
+
+        if (isFirstTime) {
+            sidebar.scrollTop = scrollTop({
+                currentScrollTop: 0,
+                scrollDiff: scrollDiff(props, threshold),
+            })
+            return
+        }
+
+        const scrollUnder = scrollDiff(props, threshold)
+        if (scrollUnder < 0) {
+            sidebar.scrollTop = scrollTop({
+                currentScrollTop: sidebar.scrollTop,
+                scrollDiff: scrollUnder,
+            })
+            return
+        }
+
+        const scrollOver = scrollDiff(props, 1 - threshold)
+        if (scrollOver > 0) {
+            sidebar.scrollTop = scrollTop({
+                currentScrollTop: sidebar.scrollTop,
+                scrollDiff: scrollOver,
+            })
+            return
+        }
+    }
+
+    type ScrollDiffProps = Readonly<{
+        sidebar: DOMRect
+        focused: DOMRect
+    }>
+    function scrollDiff({ sidebar, focused }: ScrollDiffProps, threshold: number): number {
+        return focused.top - (sidebar.top + sidebar.height * threshold)
+    }
+    function scrollTop({
+        currentScrollTop,
+        scrollDiff,
+    }: Readonly<{ currentScrollTop: number; scrollDiff: number }>): number {
+        const scrollTop = currentScrollTop + scrollDiff
+        if (scrollTop < 0) {
+            return 0
+        } else {
+            return scrollTop
+        }
+    }
+}
