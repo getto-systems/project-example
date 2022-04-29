4b0a3d50b1e96df097a75e24d9556c63fd33186f
shun <shun.fix9@gmail.com>
2022-04-29 23:54:43 +0000

    fix: test

--- api/ci/setup-test.sh
+++ api/ci/setup-test.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
 
 setup_main() {
-    setup_toolchain
+  local target
+  target=$1
+  if [ -z "$target" ]: then
+    echo "usage: setup-test.sh <install-dir>"
+    exit 1
+  fi
+  mkdir -p $target
+
+  install_cargo_llvm_cov
 }
-setup_toolchain() {
-    rustup install nightly
-    rustup component add --toolchain nightly llvm-tools-preview
-    rustup component add --toolchain nightly rustfmt
-    cargo install rustfilt
-    mkdir tmp
+install_cargo_llvm_cov() {
+  local version
+  version=$(
+    curl --silent "https://api.github.com/repos/taiki-e/cargo-llvm-cov/releases/latest" | \
+    grep '"tag_name":' | \
+    sed -E 's/.*"v([^"]+)".*/\1/' \
+  )
+
+  curl -L -o $target/cargo-llvm-cov.tar.gz https://github.com/taiki-e/cargo-llvm-cov/releases/download/v${version}/cargo-llvm-cov-aarch64-unknown-linux-gnu.tar.gz
+  cd $target
+  tar xvzf cargo-llvm-cov.tar.gz
+  rm cargo-llvm-cov.tar.gz
+  cd -
 }
 
-setup_main
+setup_main "$@"
