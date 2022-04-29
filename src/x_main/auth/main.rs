51c6284c865ce547cc9c814740d9db1f9fd05aaa
shun <shun.fix9@gmail.com>
2022-04-29 13:13:24 +0000

    fix: test

--- src/x_main/auth/main.rs
+++ src/x_main/auth/main.rs
@@ -11,6 +11,7 @@ lazy_static! {
 
 #[tokio::main]
 async fn main() {
+    // main
     let feature: Arc<AuthAppFeature> = Arc::new(AuthAppFeature::new(&ENV));
 
     let server = route::Server::new();
