ab6f966be704f5467e25b3699cca536f74dc158e
shun <shun.fix9@gmail.com>
2022-04-30 00:01:40 +0000

    fix: test

--- api/ci/setup-test.sh
+++ api/ci/setup-test.sh
@@ -19,7 +19,7 @@ install_cargo_llvm_cov() {
     sed -E 's/.*"v([^"]+)".*/\1/' \
   )
 
-  curl -L -o $target/cargo-llvm-cov.tar.gz https://github.com/taiki-e/cargo-llvm-cov/releases/download/v${version}/cargo-llvm-cov-aarch64-unknown-linux-gnu.tar.gz
+  curl -L -o $target/cargo-llvm-cov.tar.gz https://github.com/taiki-e/cargo-llvm-cov/releases/download/v${version}/cargo-llvm-cov-x86_64-unknown-linux-musl.tar.gz
   cd $target
   tar xvzf cargo-llvm-cov.tar.gz
   rm cargo-llvm-cov.tar.gz
