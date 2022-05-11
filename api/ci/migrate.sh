2c8078760d10c3cb3e62764f3b032c80c90f7034
shun <shun.fix9@gmail.com>
2022-05-11 15:28:36 +0900

    fix: migrate.sh

--- api/ci/migrate.sh
+++ api/ci/migrate.sh
@@ -2,7 +2,6 @@
 
 migrate_main() {
     local cloudsql_proxy
-
     cloudsql_proxy=$1
 
     if [ ! -x "${cloudsql_proxy}" ]; then
@@ -14,15 +13,14 @@ migrate_main() {
         echo "SQL_INSTANCE is not specified"
         exit 1
     fi
-
     if [ -z "${SQL_CREDENTIAL}" ]; then
         echo "SQL_CREDENTIAL is not specified"
         exit 1
     fi
 
-    $cloudsql_proxy -instances=${SQL_INSTANCE}=tcp:0.0.0.0:3306 -credential_file=${SQL_CREDENTIAL} &
+    #$cloudsql_proxy -instances=${SQL_INSTANCE}=tcp:0.0.0.0:3306 -credential_file=${SQL_CREDENTIAL} &
 
-    npx prisma migrate deploy --schema=src/auth/x_prisma/schema.prisma
+    #npx prisma migrate deploy --schema=src/auth/x_prisma/schema.prisma
 }
 
 migrate_main $@
