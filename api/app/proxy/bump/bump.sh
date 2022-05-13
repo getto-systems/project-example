ba73f5463a7bbe263071a64b3a30e8d040604300
shun <shun.fix9@gmail.com>
2022-05-13 19:07:30 +0900

    fix: bump version

--- api/app/proxy/bump/bump.sh
+++ api/app/proxy/bump/bump.sh
@@ -1,4 +1,4 @@
 bump_build
-bump_sync Cargo.toml 's/^version = "[0-9.-]\+-api"/version = "'$(cat $BUMP_VERSION_FILE)'"/'
-bump_sync api/vendor/getto-application/Cargo.toml 's/^version = "[0-9.-]\+-api"/version = "'$(cat $BUMP_VERSION_FILE)'"/'
-bump_sync api/vendor/aws-cloudfront-cookie/Cargo.toml 's/^version = "[0-9.-]\+-api"/version = "'$(cat $BUMP_VERSION_FILE)'"/'
+for target in $(find . -name Cargo.toml); do
+  bump_sync $target 's/^version = "[0-9.-]\+-proxy"/version = "'$(cat $BUMP_VERSION_FILE)'"/'
+done
