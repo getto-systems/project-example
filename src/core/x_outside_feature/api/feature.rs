a3bdf46b7cc69faa09724119e2f29dfa7c994fd7
shun <shun.fix9@gmail.com>
2022-05-19 03:20:09 +0900

    remove: unused files

--- src/core/x_outside_feature/api/feature.rs
+++
@@ -1,6 +0,0 @@
-use crate::z_lib::service::x_outside_feature::feature::GoogleServiceAuthorizerOutsideFeature;
-
-pub struct CommonOutsideService {
-    pub service_url: &'static str,
-    pub google_authorizer: GoogleServiceAuthorizerOutsideFeature,
-}
