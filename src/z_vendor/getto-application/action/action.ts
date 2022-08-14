13303b86dc6c6e8062153430386fb399caff6796
shun <shun.fix9@gmail.com>
2022-08-15 02:27:54 +0900

    refactor: convert class

--- src/z_vendor/getto-application/action/action.ts
+++ src/z_vendor/getto-application/action/action.ts
@@ -16,48 +16,37 @@ export function initApplicationState<S>(props: ApplicationStateProps<S>): Readon
     state: ApplicationState<S>
     post: Post<S>
 }> {
-    const action = new State(props)
+    let state = props.initialState
+    let handlers: ApplicationStateHandler<S>[] = []
+
     return {
-        state: action,
-        post: (state) => action.post(state),
-    }
-}
-
-class State<S> implements ApplicationState<S> {
-    handlers: ApplicationStateHandler<S>[] = []
-
-    readonly ignitionState: Promise<S>
-    state: S
-
-    constructor(props: ApplicationStateProps<S>) {
-        this.ignitionState = ignite()
-        this.state = props.initialState
-
-        function ignite(): Promise<S> {
-            return new Promise((resolve) => {
+        state: {
+            ignitionState: new Promise((resolve) => {
                 // コンストラクタが重くならないように初期 action は setTimeout で呼び出す
                 // 状態は currentState() で最新のものを参照するので subscribe を待つ必要はない
                 setTimeout(async () => {
                     resolve(props.ignite ? await props.ignite() : props.initialState)
                 }, 0)
-            })
-        }
+            }),
+
+            subscribe(handler: ApplicationStateHandler<S>): void {
+                handlers = [...handlers, handler]
+            },
+            unsubscribe(target: ApplicationStateHandler<S>): void {
+                handlers = handlers.filter((handler) => handler !== target)
+            },
+
+            currentState(): S {
+                return state
+            },
+        },
+        post,
     }
 
-    subscribe(handler: ApplicationStateHandler<S>): void {
-        this.handlers = [...this.handlers, handler]
-    }
-    unsubscribe(target: ApplicationStateHandler<S>): void {
-        this.handlers = this.handlers.filter((handler) => handler !== target)
-    }
-    post(state: S): S {
-        this.state = state
-        this.handlers.forEach((post) => post(state))
-        return state
-    }
-
-    currentState(): S {
-        return this.state
+    function post(newState: S): S {
+        state = newState
+        handlers.forEach((post) => post(newState))
+        return newState
     }
 }
 
